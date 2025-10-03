4314a645d95f81364b9984c895f07ea1226bdff0
fuggy <45698918+nonetrix@users.noreply.github.com>
2025-10-03 18:55:23 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yay -S \
+  ark \
+  base-devel \
+  btop \
+  firefox \
+  fzf \
+  git \
+  imv \
+  krita \
+  llama.cpp-hip \
+  nano \
+  neovim \
+  noto-fonts-cjk \
+  noto-fonts-emoji \
+  noto-fonts-extra \
+  npm \
+  obs-studio \
+  ollama-rocm \
+  prismlauncher \
+  protonup-qt-bin \
+  spotify \
+  steam \
+  vulkan-radeon \
+  yazi \
+  mpv \
+  mpvpaper
