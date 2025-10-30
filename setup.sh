139f8dc52f1b77fa33c31f224d3864b8f1c1f72e
fuggy <45698918+nonetrix@users.noreply.github.com>
2025-10-30 16:44:41 -0500

    Replace Firefox with Brave and update monitor config to fix Chromium Browsers

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ yay -S --noconfirm \
   ark \
   base-devel \
   btop \
-  firefox \
+  brave-bin \
   fzf \
   git \
   imv \
@@ -132,7 +132,7 @@ else
 
 # MONITOR-SETUP-APPLIED
 # The following lines were added by a script to configure the dual monitor layout.
-monitor=DP-1,1920x1080@165,0x0,1
+monitor=DP-1,1920x1080@165,0x0,1,vrr,0
 monitor=DP-3,1920x1200@60,1920x0,1
 EOF
     echo "Monitor configuration applied successfully."
